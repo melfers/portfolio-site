3ce0a9edb2901876398ebe08b92c517318e2d7ba
Molly Elfers <elfers.molly@gmail.com>
2019-05-07 14:09:28 -0700

    implemented autoscroll for nav

--- scripts.js
+++ scripts.js
@@ -9,3 +9,39 @@ hamburger.addEventListener("click", function() {
     topNav.className = "topNav";
   }
 });
+
+function debounce(func, wait, immediate) {
+  var timeout;
+  return function() {
+    var context = this,
+      args = arguments;
+    var later = function() {
+      timeout = null;
+      if (!immediate) func.apply(context, args);
+    };
+    var callNow = immediate && !timeout;
+    clearTimeout(timeout);
+    timeout = setTimeout(later, wait);
+    if (callNow) func.apply(context, args);
+  };
+}
+
+function autoScroll(selector) {
+  var scrollAttempts = 0;
+  var incrementScrollAttempts = debounce(function() {
+    scrollAttempts++;
+  });
+
+  window.addEventListener("scroll", incrementScrollAttempts);
+
+  var el = document.querySelector(selector);
+  var chkReadyState = setInterval(function() {
+    if (el) {
+      window.scrollTo(0, el.offsetTop);
+    }
+    if (document.readyState == "complete" || scrollAttempts > 1) {
+      clearInterval(chkReadyState);
+      window.removeEventListener("scroll", incrementScrollAttempts, false);
+    }
+  }, 100);
+}
